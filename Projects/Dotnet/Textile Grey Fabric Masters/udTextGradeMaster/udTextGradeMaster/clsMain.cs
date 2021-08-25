acffd1ad339fe006d1cc961943c1d7aa7659cbe6
pgbhoir <pgbhoir@udyogsoftware.com>
2021-08-25 17:11:37 +0530

    Updated till 23/08/2021

---
+++ Projects/Dotnet/Textile Grey Fabric Masters/udTextGradeMaster/udTextGradeMaster/clsMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udTextGradeMaster
+{
+    static class clsMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                //args = new string[] { "1", "U011920", "AIPLLTM035\\SQLEXPRESS2014", "sa", "sa@1985r2", "^21001", "ADMIN", @"D:\UdyogERP\Bmp\Icon_VudyogGST.ico", "UdyogERPSDK", "UdyogERPSDK", "1", "udpid6096DTM20110307112715", "ADMINISTRATOR", "" };
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain(args));
+        }
+    }
+}
