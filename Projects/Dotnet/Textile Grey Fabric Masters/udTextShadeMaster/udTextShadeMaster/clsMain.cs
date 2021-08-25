bcb7305e4d8df06a044d13867847aec1fe67bca2
pgbhoir <pgbhoir@udyogsoftware.com>
2021-08-25 17:14:29 +0530

    Updated till 23/08/2021

---
+++ Projects/Dotnet/Textile Grey Fabric Masters/udTextShadeMaster/udTextShadeMaster/clsMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udTextShadeMaster
+{
+    static class clsMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                //args = new string[] { "13", "U091920", "aipldtm002,1433", "sa", "sa1985", "^21001", "ADMIN", @"D:\Installer\UdyogERPSDK_2.2.0\Bmp\Icon_VudyogGST.ico", "UdyogERPSDK", "UdyogERPSDK", "1", "udpid6096DTM20110307112715", "ADMINISTRATOR", "" };
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain(args));
+        }
+    }
+}
