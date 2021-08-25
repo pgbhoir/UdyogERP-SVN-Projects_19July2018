ef1bf7805952e9035979ff9cf61bf90aed8d6190
pgbhoir <pgbhoir@udyogsoftware.com>
2021-08-25 17:39:54 +0530

    Updated till 23/08/2021

--- Projects/Dotnet/udAdditionalInfo/Backup/udAdditionalInfo/udAdditionalInfo.cs
+++ Projects/Dotnet/udAdditionalInfo/Backup/udAdditionalInfo/udAdditionalInfo.cs
@@ -15,7 +15,7 @@ namespace udAdditionalInfo
         public string _EntryTy;
         public string _TblName;
         public DataSet _commonDs;
-        public string _FilterRowId=string.Empty;
+
         public udAdditionalInfo()
         { 
             // Default Constructor
@@ -23,15 +23,14 @@ namespace udAdditionalInfo
 
         public void callAdditionalInfo()
         {
-            _oDataAccess = new clsDataAccess();
+            //_oDataAccess = new clsDataAccess();
 
             _dtLother = new DataTable();
             DataRow[] _dr;
             string cSql;
             //cSql = "Select * from Lother Where E_Code='"+_EntryTy+"' and Att_File="+_HdrDtl=="H" ? "1" : "0" + " order by Formno, Serial, SubSerial ";
             //_dtLother = _oDataAccess.GetDataTable(cSql, null, 25);
-            //_dr = _commonDs.Tables["Lother"].Select(_HdrDtl=="H"? " Att_file=1 ":"Att_File=0");
-            _dr = _commonDs.Tables["Lother"].Select((_HdrDtl == "H" ? " Att_file=1 " : "Att_File=0")+" and Ingrid=0 ");    // Changed by Sachin N. S. on 19/11/2018 for Pharma Retailer Changes
+            _dr = _commonDs.Tables["Lother"].Select(_HdrDtl=="H"? " Att_file=1 ":"Att_File=0");
             if (_dr.Count() > 0)
                 _dtLother = _dr.CopyToDataTable();
             else
@@ -39,8 +38,7 @@ namespace udAdditionalInfo
 
             if (_dtLother.Rows.Count > 0)
             {
-                //udFrmAdditionalInfo _udFrmAdditionalInfo = new udFrmAdditionalInfo(_dtLother,_HdrDtl,_TblName,_commonDs);
-                udFrmAdditionalInfo _udFrmAdditionalInfo = new udFrmAdditionalInfo(_dtLother, _HdrDtl, _TblName, _commonDs, _FilterRowId);
+                udFrmAdditionalInfo _udFrmAdditionalInfo = new udFrmAdditionalInfo(_dtLother,_HdrDtl,_TblName,_commonDs);
                 _udFrmAdditionalInfo.Show();
             }
         }
